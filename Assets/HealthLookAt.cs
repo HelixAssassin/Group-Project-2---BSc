a6eaf324d5ce1691fa63024b195779b9d14c9c56
ernest68680 <44022033+ernest68680@users.noreply.github.com>
2019-05-13 18:31:09 +0100

    Update (Ernest)
    UI changed, hands added.

---
+++ Assets/HealthLookAt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthLookAt : MonoBehaviour
+{
+    [SerializeField] Camera playerCamera;
+
+
+	void Update ()
+    {
+        transform.LookAt(playerCamera.transform.position);
+	}
+}

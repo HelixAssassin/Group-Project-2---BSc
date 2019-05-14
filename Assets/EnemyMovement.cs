50c3fbb0e2e2707318eac36193a7016cc36939ae
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2019-05-14 11:18:31 +0100

    Enemy Changes
    Mde changes to the enemy game objects

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    Transform playerModel;
+    CharacterController controller;
+
+    // This allows the character object to move, jump and be affected by gravity
+    [SerializeField] float jumpSpeed = 20.0f;
+    [SerializeField] float gravity = 1.0f;
+    float yVelocity = 0.0f;
+
+    [SerializeField] float moveSpeed = 5.0f;
+    public float h;
+    public float v;
+
+    // Use this for initialization
+    void Start () {
+        // This code segment allows the enemy game object to follow an object that is tagged with player.
+        GameObject playerGameObject = GameObject.FindGameObjectWithTag("Player");
+        playerModel = playerGameObject.transform;
+        controller = GetComponent<CharacterController>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(h, 0, v);
+        Vector3 velocity = direction * moveSpeed;
+
+        Vector3 dircetion = playerModel.position - transform.position;
+        controller.Move(dircetion * Time.deltaTime);
+		
+	}
+}

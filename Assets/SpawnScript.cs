328c07ec650109ec4d31d556f985db1eda3c1aff
Shaan Singh <44022060+HelixAssassin@users.noreply.github.com>
2019-05-24 13:01:38 +0100

    Fixes
    Just adding some small fixes.

--- Assets/SpawnScript.cs
+++ Assets/SpawnScript.cs
@@ -2,29 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnScript : MonoBehaviour
-{
+public class SpawnScript : MonoBehaviour {
 
-    [SerializeField] GameObject thingToSpawn;
-    [SerializeField] float delayBetweenSpawns = 2.0f;
-    [SerializeField] float timeOfNextSpawn = 1f;
-    int amountToSpawn = 10;
-    static int amountSpawned = 1;
+	[SerializeField] GameObject thingToSpawn;
+	[SerializeField] float delayBetweenSpawns=2.0f;
+	[SerializeField] float timeOfNextSpawn=1f;
+	int amountToSpawn=20;
+	static int amountSpawned=1;
+	int currentHealth;
+	
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Time.time >= timeOfNextSpawn && amountSpawned < amountToSpawn)
-        {
-            Instantiate(thingToSpawn, transform.position, Quaternion.identity);
-            timeOfNextSpawn = Time.time + delayBetweenSpawns;
-            amountSpawned++;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time >= timeOfNextSpawn&&amountSpawned<amountToSpawn)
+		{
+			Instantiate(thingToSpawn,transform.position,Quaternion.identity);
+			timeOfNextSpawn = Time.time + delayBetweenSpawns;
+			amountSpawned++;
+		}
+	}
 
-        }
-    }
-       static public void EnemyDie()
-        {
-            amountSpawned--;
-        }
-
-    }
+	static public void EnemyDie()
+		{
+			amountSpawned--;
+		}
+}
